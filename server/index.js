657525449827b409f679c99816f4a7704f1fd05c
rahul8279 <rana827965@gmail.com>
2025-07-05 10:41:35 +0530

    code

--- server/index.js
+++ server/index.js
@@ -32,6 +32,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
+
+
 app.use('/api/v1/user',userRoutes)
 app.use('/api/v1/bus', busRoutes);
 app.use('/api/v1/route', routeRoutes);
